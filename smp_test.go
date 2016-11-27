dd63780bf55f8da6145a63af702d3175ecfba621
Blake Griffith <cowlicks@riseup.net>
2016-11-27 15:21:48 -0600

    add test for bad person

--- smp_test.go
+++ smp_test.go
@@ -60,7 +60,7 @@ func TestBobLies(t *testing.T) {
 	}
 }
 
-func TestPerson(t *testing.T) {
+func TestGoodPerson(t *testing.T) {
 	pub := NewPublic()
 	msg := []byte("El pueblo unido")
 
@@ -83,5 +83,30 @@ func TestPerson(t *testing.T) {
 	if !alice.Check() {
 		t.Fatal()
 	}
+}
 
+func TestBadPerson(t *testing.T) {
+	alicesecret := []byte("No pasaran")
+	bobsecret := []byte("la tumba de facismo")
+	pub := NewPublic()
+
+	alice := NewPerson(pub, alicesecret)
+	bob := NewPerson(pub, bobsecret)
+
+	alice.FirstKeyReceive(bob.FirstKeySend())
+	bob.FirstKeyReceive(alice.FirstKeySend())
+
+	alice.SecondReceive(bob.SecondSend())
+	bob.SecondReceive(alice.SecondSend())
+
+	alice.FinalReceive(bob.FinalSend())
+	bob.FinalReceive(alice.FinalSend())
+
+	if bob.Check() {
+	   t.Fatal()
+	}
+
+	if alice.Check() {
+		t.Fatal()
+	}
 }
